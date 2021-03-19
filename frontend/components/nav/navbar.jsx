f8f830a278fa2c8ec0f2bfc3bc25b706f7dee00a
christian <christian.wooddell@rochester.edu>
2021-03-19 09:00:10 -0400

    styling updates

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -6,19 +6,27 @@ import { faSpotify } from "@fortawesome/free-brands-svg-icons";
 
 const NavBar = ({ currentUser, logout }) => {
     const sessionLinks = () => (
+        <header>
         <nav className="login-signup">
             <FontAwesomeIcon icon={faSpotify} className="spotify-logo" />
-            <Link to="/login">Log in.</Link>
-            <Link to="/signup">Sign up.</Link>
+            <a href="https://www.linkedin.com/in/christian-wooddell/">Linkedin</a>
+            <a href="https://github.com/christianwooddell">Github</a>
+            <a href="">Portfolio</a>
+
+            <Link to="/login">Log in</Link>
+            <Link to="/signup">Sign up</Link>
         </nav>
+        </header>
     );
 
     const personalGreeting = () => (
+        <header>
         <hgroup className="header-group">
             <FontAwesomeIcon icon={faSpotify} className="spotify-logo" />
             <h2 className="header-name">Hi, {currentUser.username}!</h2>
             <button className="header-button" onClick={logout}>Log Out</button>
         </hgroup>
+        </header>
     );
 
     return currentUser ? personalGreeting() : sessionLinks();
