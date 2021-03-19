e2eb530bdaa03ba17af209d9531460f18a61ab73
christian <christian.wooddell@rochester.edu>
2021-03-18 20:07:43 -0400

    add logo to navbar

--- frontend/components/nav/navbar.jsx
+++ frontend/components/nav/navbar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+
 
 const NavBar = ({ currentUser, logout }) => {
     const sessionLinks = () => (
         <nav className="login-signup">
-
+            <FontAwesomeIcon icon={faSpotify} className="spotify-logo" />
             <Link to="/login">Log in.</Link>
             <Link to="/signup">Sign up.</Link>
         </nav>
@@ -12,6 +15,7 @@ const NavBar = ({ currentUser, logout }) => {
 
     const personalGreeting = () => (
         <hgroup className="header-group">
+            <FontAwesomeIcon icon={faSpotify} className="spotify-logo" />
             <h2 className="header-name">Hi, {currentUser.username}!</h2>
             <button className="header-button" onClick={logout}>Log Out</button>
         </hgroup>
