f2e9391e6b736bbd56b88a97528fda8b23413cb7
christian <christian.wooddell@rochester.edu>
2021-03-19 20:40:55 -0400

    logout function

--- frontend/components/home_page/home_page.jsx
+++ frontend/components/home_page/home_page.jsx
@@ -2,9 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.logoutUser = this.logoutUser.bind(this);
+    }
+    logoutUser(){
+        this.props.logout()
+            .then(()=> this.props.history.push("/"));
+    }
     render() {
+        const { currentUser } = this.props;
         return(
-            <h1>look at all these songs bestie!</h1>
+            <div>
+                <h1>look at all these songs bestie!</h1>
+                <button className="logout" onClick={this.logoutUser()}>Log out</button>
+            </div>
         )
     }
 }
