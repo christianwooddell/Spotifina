b15aed1739fa845ac6b9e28697d07976d1597874
christian <christian.wooddell@rochester.edu>
2021-03-25 17:02:49 -0400

    styling changes and heroku push

--- app/controllers/api/albums_controller.rb
+++ app/controllers/api/albums_controller.rb
@@ -1,13 +1,13 @@
-class Api::AlbumsController < ApplicationController 
+# class Api::AlbumsController < ApplicationController 
 
-    def show 
-        @album = Album.includes(:artist, :songs).with_attached_album_cover.find(params[:id])
-        if @album 
-            @songs = @album.songs.with_attached_audio_file
-            render :show 
-        else
-            render json: @album.errors.full_messages, status: 422 
-        end 
-    end 
+#     def show 
+#         @album = Album.includes(:artist, :songs).with_attached_album_cover.find(params[:id])
+#         if @album 
+#             @songs = @album.songs.with_attached_audio_file
+#             render :show 
+#         else
+#             render json: @album.errors.full_messages, status: 422 
+#         end 
+#     end 
 
-end     
+# end     
--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,13 +1,13 @@
-class Api::ArtistsController < ApplicationController 
+# class Api::ArtistsController < ApplicationController 
 
-    def show 
-        @artist = Artist.includes(:album, :songs).with_attached_artist_image.find(params[:id])
-        if @artist 
-            @albums = @artist.albums.includes(:songs).with_attached_album_cover
-            render :show 
-        else
-            render json: @artist.errors.full_messages, status: 422 
-        end 
-    end 
-    
-end 
+#     def show 
+#         @artist = Artist.includes(:album, :songs).with_attached_artist_image.find(params[:id])
+#         if @artist 
+#             @albums = @artist.albums.includes(:songs).with_attached_album_cover
+#             render :show 
+#         else
+#             render json: @artist.errors.full_messages, status: 422 
+#         end 
+#     end 
+
+# end 
